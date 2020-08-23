edceb05a40c805a2c8f0719be0e1737f21a5fa69
abelcerda <abelantonio89@gmail.com>
2020-08-23 13:37:32 -0300

    Added Material Icons library. Fixed Navbar listed items.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,9 +9,17 @@ const Navbar = () => {
           Shopping
         </Link>
         <ul className="right">
-          <Link to="/">Shop</Link>
-          <Link to="/cart">My cart</Link>
-          <Link to="/cart">shopping_cart</Link>
+          <li>
+            <Link to="/">Shop</Link>
+          </li>
+          <li>
+            <Link to="/cart">My cart</Link>
+          </li>
+          <li>
+            <Link to="/cart">
+              <i className="material-icons">shopping_cart</i>
+            </Link>
+          </li>
         </ul>
       </div>
     </nav>
