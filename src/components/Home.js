2fbd2697ec8fcfb85672896d0cc0d63b0d62efe2
abelcerda <abelantonio89@gmail.com>
2020-08-23 13:46:32 -0300

    Added logic to display the items in the Home component. Added styling to display small cards.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,41 @@
 import React from "react";
+import { connect } from "react-redux";
+import "./Home.css";
 
-const Home = () => {
+const Home = ({ items }) => {
+  const itemList = items.map((item) => {
+    return (
+      <div className="card" key={item.id}>
+        <div className="card-image">
+          <img src={item.img} alt={item.title} />
+          <span className="card-title">{item.title}</span>
+          <span
+            to="/"
+            className="btn-floating halfway-fab waves-effect waves-light red"
+          >
+            <i className="material-icons">add</i>
+          </span>
+        </div>
+        <div className="card-content">
+          <p>{item.desc}</p>
+          <p>
+            <b>Price: {item.price}$</b>
+          </p>
+        </div>
+      </div>
+    );
+  });
+
   return (
     <div className="container">
-      <h3>Home</h3>
+      <h3 className="center">Our items</h3>
+      <div className="box">{itemList}</div>
     </div>
   );
 };
 
-export default Home;
+const mapStateToProps = (state) => {
+  return { items: state.items };
+};
+
+export default connect(mapStateToProps)(Home);
