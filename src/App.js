f1ae9795735e7b69b131bfeea850204bea63a1df
abelcerda <abelantonio89@gmail.com>
2020-08-23 13:22:57 -0300

    Added Home component with placeholder content. Replaced default app content with Home component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
-import Navbar from "./components/Navbar";
 import { BrowserRouter } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
 
 class App extends Component {
   render() {
@@ -9,13 +9,7 @@ class App extends Component {
       <BrowserRouter>
         <div className="App">
           <Navbar />
-          <div className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <h2>Welcome to React</h2>
-          </div>
-          <p className="App-intro">
-            To get started, edit <code>src/App.js</code> and save to reload.
-          </p>
+          <Home />
         </div>
       </BrowserRouter>
     );
